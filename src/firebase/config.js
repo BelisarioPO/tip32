8fc35e4a9ebee00dccbe25ba3fd71a9c3e84896d
BelisarioPO <belisarioperezotero03@gmail.com>
2023-10-30 11:59:34 -0300

    Comienzo de una nueva era

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_rpUOPCcf4zqXYAR2-XHf17uy-MR9znE",
+    authDomain: "tip-iii-2.firebaseapp.com",
+    projectId: "tip-iii-2",
+    storageBucket: "tip-iii-2.appspot.com",
+    messagingSenderId: "310045260520",
+    appId: "1:310045260520:web:b73032d742d4659527e75d"
+  };
