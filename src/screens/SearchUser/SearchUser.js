9256c749d119ad63359fd49adac17dda38b21654
TaylorTomas <ttaylor@udesa.edu.ar>
2023-11-13 12:38:20 -0300

    searchUsers finito

--- src/screens/SearchUser/SearchUser.js
+++ src/screens/SearchUser/SearchUser.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, FlatList } from 'react-native';
 import { db } from '../../firebase/config';
+import { useRoute } from '@react-navigation/native';
 
 class SearchUser extends Component {
   constructor() {
@@ -8,47 +9,56 @@ class SearchUser extends Component {
     this.state = {
       searchQuery: '',
       searchResults: [],
+      todosUsuariosCDM: [],
     };
   }
 
+componentDidMount(){
+
+  db.collection('users').onSnapshot(
+    usuarios => {
+      let todosUsuarios = [];
+      usuarios.forEach((doc) => {
+        const data = doc.data();
+        todosUsuarios.push({
+          id: doc.id,
+          userName: data.userName,
+        });
+      });
+
+      this.setState({ todosUsuariosCDM: todosUsuarios })
+      console.log(this.state.todosUsuariosCDM)
+    }
+  )
+}
+
+
   searchUsers = (text) => {
 
-    this.setState({ searchQuery: text })
+    this.state.todosUsuariosCDM.forEach(unUser => {
 
-    const usersDb = db.collection('users');
+      if (this.state.searchQuery.length==0){
+        this.setState({
+          searchResults: []
+        })
+      }
+      if (unUser.userName.includes(this.state.searchQuery)){
+        if (this.state.searchResults.includes(unUser))
+        {null}
+        else{this.state.searchResults.push(unUser)}
+      }
 
-    usersDb
-      .where('userName', '==', text) // Filtrar por user name
-      .onSnapshot((querySnapshot) => {
-        console.log(querySnapshot.metadata);
-        const results = [];
+    })
 
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          results.push({
-            id: doc.id,
-            userName: data.userName,
-          });
-        });
-        console.log('test', results);
-        if (results.length > 0) {
-          this.setState({ searchResults: results });
-        } else {
-          this.setState({ searchResults: [] });
-        }
-      })
-      // .catch((error) => {
-      //   console.error('Error al buscar usuarios:', error);
-      // });
-  };
+  }
 
   render() {
-    console.log(this.state.searchResults);
+    console.log(this.state.todosUsuariosCDM);
     return (
       <View>
         <TextInput
           placeholder="Buscar usuario por user name"
-          onChangeText={(text) => this.searchUsers(text)}
+          onChangeText={(text) => (this.searchUsers(text), this.setState({searchQuery:text}))}
           value={this.state.searchQuery}
         />
         {this.state.searchResults.length === 0 ? (
