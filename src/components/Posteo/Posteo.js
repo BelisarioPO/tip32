b71dd5cb82f5a88ab44d63a86ca234aa288610f8
Tomas Ruiz <truiz@consultatioinvestments.com>
2023-11-17 13:50:56 -0300

    CSS

--- src/components/Posteo/Posteo.js
+++ src/components/Posteo/Posteo.js
@@ -1,19 +1,19 @@
 import react, { Component } from 'react';
-import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList,Image} from 'react-native';
+import { TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import { db, auth } from '../../firebase/config';
 import firebase from 'firebase';
 
 class Posteo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             like: false,
             cantidadDeLikes: this.props.infoPost.data.likes
         }
     }
 
-    componentDidMount(){
-        if(this.props.infoPost.data.likes.includes(auth.currentUser.email)){
+    componentDidMount() {
+        if (this.props.infoPost.data.likes.includes(auth.currentUser.email)) {
             this.setState({
                 like: true
             })
@@ -21,67 +21,104 @@ class Posteo extends Component {
     }
 
 
-   likear(){
-    db.collection('posts').doc(this.props.infoPost.id).update({
-        likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
-    })
-    .then( res => {
-        this.setState({
-            like: true,
-            cantidadDeLikes: this.props.infoPost.data.likes.length
+    likear() {
+        db.collection('posts').doc(this.props.infoPost.id).update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
         })
-    })
-    .catch( e => console.log(e))
+            .then(res => {
+                this.setState({
+                    like: true,
+                    cantidadDeLikes: this.props.infoPost.data.likes.length
+                })
+            })
+            .catch(e => console.log(e))
 
 
-   }
+    }
 
-   unLike(){
-    db.collection('posts').doc(this.props.infoPost.id).update({
-        likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
-    })
-    .then( res => {
-        this.setState({
-            like: false,
-            cantidadDeLikes: this.props.infoPost.data.likes.length
+    unLike() {
+        db.collection('posts').doc(this.props.infoPost.id).update({
+            likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
         })
-    })
-    .catch( e => console.log(e))
-   }
-   
+            .then(res => {
+                this.setState({
+                    like: false,
+                    cantidadDeLikes: this.props.infoPost.data.likes.length
+                })
+            })
+            .catch(e => console.log(e))
+    }
 
-    render(){
+
+    render() {
         console.log(this.props);
-        return(
-            <View>
-                <Text>Datos del Post</Text>
-                <Text> Email: {this.props.infoPost.data.owner}</Text>
-                <Text>Texto: {this.props.infoPost.data.textoPost}</Text>
-                <Text>cantidad de likes: {this.state.cantidadDeLikes}</Text>
-                <Image style={styles.imagen} source={this.props.infoPost.data.fotoUrl} resizeMode='contain'/>
+        return (
+            <View style={styles.postContainer}>
+                <Text style={styles.title}>Datos del Post</Text>
+                <Text style={styles.text}>Email: {this.props.infoPost.data.owner}</Text>
+                <Text style={styles.text}>Texto: {this.props.infoPost.data.textoPost}</Text>
+                <Text style={styles.text}>Cantidad de likes: {this.state.cantidadDeLikes}</Text>
+                <Image style={styles.imagen} source={this.props.infoPost.data.fotoUrl} resizeMode='contain' />
 
                 {/* If ternario */}
-                {this.state.like ? 
-                <TouchableOpacity onPress={()=>this.unLike()}>
-                    QuitarLike
-                </TouchableOpacity>
-                :
-                <TouchableOpacity onPress={()=>this.likear()}>
-                    Like
-                </TouchableOpacity>
+                {this.state.like ?
+                    <TouchableOpacity style={styles.buttonLiked} onPress={() => this.unLike()}>
+                        <Text style={styles.buttonText}>Quitar Like</Text>
+                    </TouchableOpacity>
+                    :
+                    <TouchableOpacity style={styles.button} onPress={() => this.likear()}>
+                        <Text style={styles.buttonText}>Like</Text>
+                    </TouchableOpacity>
                 }
-                
-                
+
+
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
+    postContainer: {
+        backgroundColor: '#f5f5f5',
+        padding: 10,
+        margin: 10,
+        borderRadius: 8,
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
     imagen: {
         height: 100,
-        width:100
-    }
-})
+        width: 100,
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: '#3498db',
+        padding: 8,
+        borderRadius: 5,
+        marginTop: 5,
+        alignItems: 'center',
+        width: '20%'
+    },
+    buttonLiked: {
+        backgroundColor: '#e74c3c',
+        padding: 8,
+        borderRadius: 5,
+        marginTop: 5,
+        alignItems: 'center',
+        width: '20%'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+});
 
 
 
--- src/screens/Home/home.js
+++ src/screens/Home/home.js
@@ -1,86 +1,122 @@
 import react, { Component } from 'react';
-import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList} from 'react-native';
-import { db,auth } from '../../firebase/config';
+import { TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList,ScrollView } from 'react-native';
+import { db, auth } from '../../firebase/config';
 import Posteo from "../../components/Posteo/Posteo"
 
 class Home extends Component {
-    constructor(){
+    constructor() {
         super()
-        this.state={
-      listaposts: []
+        this.state = {
+            listaposts: []
         }
     }
 
-componentDidMount(){
-    //traer datos
-    db.collection('posts').onSnapshot(
-        posteos => {
-            let postsaMostrar = [];
-            posteos.forEach(Unpost => {
-                postsaMostrar.push({
-                    id: Unpost.id,
-                    data: Unpost.data()
-                })
-                this.setState({
-                    listaposts: postsaMostrar,
-        
+    componentDidMount() {
+        //traer datos
+        db.collection('posts').onSnapshot(
+            posteos => {
+                let postsaMostrar = [];
+                posteos.forEach(Unpost => {
+                    postsaMostrar.push({
+                        id: Unpost.id,
+                        data: Unpost.data()
+                    })
+                    this.setState({
+                        listaposts: postsaMostrar,
+
+                    })
                 })
             })
-        })
-}
+    }
 
 
-    logout(){
+    logout() {
         auth.signOut();
-         //Redirigir al usuario a la home del sitio.
+        //Redirigir al usuario a la home del sitio.
         this.props.navigation.navigate('Login')
     }
 
-    render(){
-        return(
-            <View>
-                <Text>HOME</Text>
-                <TouchableOpacity onPress={()=>this.props.navigation.navigate("Login")}>
-                    <Text> Ir a login</Text>
+    render() {
+        return (
+            <ScrollView contentContainerStyle={styles.container}>
+                <Text style={styles.headerText}>HOME</Text>
+                <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate("Login")}>
+                    <Text style={styles.buttonText}> Ir a login</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>this.logout()}>
-                    <Text> Logout</Text>
+                <TouchableOpacity style={styles.button} onPress={() => this.logout()}>
+                    <Text style={styles.buttonText}> Logout</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>this.props.navigation.navigate("Register")}>
-                    <Text> Ir a register</Text>
+                <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate("Register")}>
+                    <Text style={styles.buttonText}> Ir a register</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={()=>this.props.navigation.navigate("Miperfil")}>
-                    <Text> Ir a Mi Perfil</Text>
+                <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate("Miperfil")}>
+                    <Text style={styles.buttonText}> Ir a Mi Perfil</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate("SearchUser")}>
-                <Text>Buscar Usuario</Text>
+                <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate("SearchUser")}>
+                    <Text style={styles.buttonText}>Buscar Usuario</Text>
                 </TouchableOpacity>
-                <Text>Lista de Posts</Text>
+                <Text style={styles.listHeader}>Lista de Posts</Text>
                 {
-                    this.state.listaposts.length === 0 
-                    ?
-                    <Text>Cargando...</Text>
-                    :
-                    <FlatList style={styles.flex}
-                        data= {this.state.listaposts}
-                        keyExtractor={ unPost => unPost.id }
-                        renderItem={ ({item}) => <Posteo infoPost = { item } /> }
-                    />
+                    this.state.listaposts.length === 0
+                        ?
+                        <Text>Cargando...</Text>
+                        :
+                        <FlatList style={styles.flatList}
+                            data={this.state.listaposts}
+                            keyExtractor={unPost => unPost.id}
+                            renderItem={({ item }) => <Posteo infoPost={item}/>}
+                        />
                 }
-            </View>
+            </ScrollView> 
+            //QUERIDOS PROFESORES, SOY FEDERICO CINQUE. YA SE QUE NUNCA LLEGAMOS A DAR 
+            //SCROLLVIEW Y QUE ESTA SACADO DE INTERNET! PERO SEGUI EL CONSEJO DE GASTON DE ASIGNARLE
+            //FLEX 1 AL CONTAINER PRINCIPAL Y AUN ASI NO PODIA SCROLEAR PARA VER MIS POSTS
+            //ESTA FUE MI UNICA SOLUCION. LO VI EN UN VIDEO DE YOUTUBE. LO IMPORTE Y LO IMPLEMENTE
+            //SOLO EN LA LINEA 41 Y 70. PERDON DESDE YA! 
+
             
+
 
         )
     }
 }
+
 const styles = StyleSheet.create({
-    formContainer: {
-        flex: 1
-    }
-})
-    
-
-
+    container: {
+        flexGrow: 1,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#eaeaea',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        marginVertical: 6,
+        borderRadius: 4,
+        alignItems: 'center',
+        width: '50%', // Ajusta el ancho deseado aquí
+    },
+    buttonText: {
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    listHeader: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    flatList: {
+        width: '100%',
+    },
+});
 
 
 export default Home;
